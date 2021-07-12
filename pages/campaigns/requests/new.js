0919d4c14ea57249a0126123b1e91ab7c81a6bf9
Maruko-coding <markojau@gmail.com>
2021-07-12 16:11:16 -0300

    cards and requests

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class requestNew extends Component {
+	render() {
+		return <h3>Create a request</h3>;
+	}
+}
+export default requestNew;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Card, Grid, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes';
 
 class CampaignShow extends Component {
 	static async getInitialProps(props) {
@@ -9,6 +13,7 @@ class CampaignShow extends Component {
 		const summary = await campaign.methods.getSummary().call();
 
 		return {
+			address: props.query.address,
 			minimumContribution: summary[0],
 			balance: summary[1],
 			requestsCount: summary[2],
@@ -17,10 +22,73 @@ class CampaignShow extends Component {
 		};
 	}
 
+	renderCards() {
+		const {
+			balance,
+			manager,
+			minimumContribution,
+			requestsCount,
+			approversCount,
+		} = this.props;
+
+		const items = [
+			{
+				header: manager,
+				meta: 'Address of Manager',
+				description:
+					'The manager created this campaign and can create requests to withdraw money',
+				style: { overflowWrap: 'break-word' },
+			},
+			{
+				header: minimumContribution,
+				meta: 'Minimum Contribution (wei)',
+				description:
+					'You must contribute at least this much wei to become a contributor',
+			},
+			{
+				header: requestsCount,
+				meta: 'Number of Requests',
+				description:
+					'A request made by the manager in order to withdraw money from the contract. Requests must be approved by contributors',
+			},
+			{
+				header: approversCount,
+				meta: 'Number of Approvers',
+				description:
+					'Number of people who have already donated to this account',
+			},
+			{
+				header: web3.utils.fromWei(balance, 'ether'),
+				meta: 'Campaign Balance (ETH)',
+				description: 'The balance that this account could spend',
+			},
+		];
+
+		return <Card.Group items={items} />;
+	}
+
 	render() {
 		return (
 			<Layout>
 				<h3>Campaign Show</h3>
+				<Grid>
+					<Grid.Row>
+						<Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+
+						<Grid.Column width={6}>
+							<ContributeForm address={this.props.address} />
+						</Grid.Column>
+					</Grid.Row>
+					<Grid.Row>
+						<Grid.Column>
+							<Link route={`campaigns/${this.props.address}/requests`}>
+								<a>
+									<Button primary>View Requests</Button>
+								</a>
+							</Link>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
 			</Layout>
 		);
 	}
